d1ef83790de2db523a81d0fe305ec240f7e8967b
Artur Pedro <arturpedromartins@gmail.com>
2025-01-09 20:12:15 -0300

    Feat: cd implemented

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-builtin_commands = ["echo","exit","type","pwd"]
+USER_NAME = "artur"
+builtin_commands = ["echo","exit","type","pwd","cd"]
 
 def type_command(type_command, all_paths=None,quiet=True):
 
@@ -26,25 +27,47 @@ def type_command(type_command, all_paths=None,quiet=True):
 
     return found, search_path
 
+def get_current_dir(absolute=False):
+    absolute_path = os.getcwd()
+    if absolute:
+        return absolute_path
+    else:
+        return "~" + absolute_path.lstrip("/home/artur")
+
+def cd(path):
+    if path.startswith("~"):
+        path = path.replace("~", "/home/artur")
+    os.chdir(path)
+
 def main():
 
     PATH = os.environ.get("PATH")
 
     while True:
-        sys.stdout.write("$ ")
-        
-        # Wait for user input
-        command = input()
-        
-        if command == "exit 0":
-            break
-        
-        user_stdin = command.split(" ")
-        main_command = user_stdin[0]
+        check_dir = get_current_dir(absolute=True)
+
+        if (check_dir.lstrip("/home/") == ""):
+            current_dir = get_current_dir(absolute=True)
+        else:
+            current_dir = get_current_dir(absolute=False)
 
         if PATH:
             all_paths = PATH.split(":")
 
+        #sys.stdout.write(f"{current_dir}$ ")
+        sys.stdout.write("$ ")
+        # Wait for user input
+        command = input()
+        
+        # Force exit
+        if command == "exit 0":
+            break
+        
+        # Parse user input
+        user_stdin = command.split(" ")
+        main_command = user_stdin[0]
+
+        # Handle builtin commands
         if main_command in builtin_commands:
             
             if main_command == "echo":
@@ -58,12 +81,17 @@ def main():
                 
             elif main_command == "pwd":
                 print(os.getcwd())
+
+            elif main_command == "cd":
+                cd(user_stdin[1])
+        # Handle external commands
         elif PATH:
             found, search_path = type_command(main_command,all_paths)
             if found:
                 os.system(command)
             else:
-                print(f"{command}: command not found")
+                print(f"{main_command}: command not found")
+
 
 if __name__ == "__main__":
     main()
