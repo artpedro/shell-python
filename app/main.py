55dcd2b465966afd22e38133105dad09013a553e
Artur Pedro <arturpedromartins@gmail.com>
2025-01-09 00:51:44 -0300

    Feat: invalid command implemented

--- app/main.py
+++ app/main.py
@@ -4,9 +4,14 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
+    
+    valid_commands = ["cd", "pwd", "echo", "exit"]
     # Wait for user input
-    input()
+    command = input()
+    if command in valid_commands:
+        print(command)
+    else:
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
