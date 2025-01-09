ff35a24b8a9d85f6696301c1ac7b83161819c08f
Artur Pedro <arturpedromartins@gmail.com>
2025-01-09 01:51:27 -0300

    Feat: running PATH commands implemented

--- app/main.py
+++ app/main.py
@@ -3,6 +3,29 @@ import os
 
 builtin_commands = ["echo","exit","type"]
 
+def type_command(type_command, all_paths=None,quiet=True):
+
+    found = False
+
+    if type_command in builtin_commands:
+        print(f"{type_command} is a shell builtin")
+        found = True
+        search_path = "shell builtin"
+
+    if (not found) and all_paths:
+        for path in all_paths:
+            search_path = f"{path}/{type_command}"
+            if os.path.isfile(search_path):
+                if not quiet:
+                    print(f"{type_command} is {search_path}")
+                found = True
+                break
+
+    if not found:
+        search_path = None
+
+    return found, search_path
+
 def main():
 
     PATH = os.environ.get("PATH")
@@ -28,24 +51,16 @@ def main():
                 print(" ".join(user_stdin[1:]))
             
             elif main_command == "type":
-                type_command = user_stdin[1]
-                found = False
-
-                if type_command in builtin_commands:
-                    print(f"{type_command} is a shell builtin")
-                    found = True
-
-                elif PATH:
-                    for path in all_paths:
-                        search_path = f"{path}/{type_command}"
-                        if os.path.isfile(search_path):
-                            print(f"{type_command} is {search_path}")
-                            found = True
-                            break
-                        else:
-                            continue
+                
+                if PATH:
+                    found, search_path = type_command(user_stdin[1],all_paths,quiet=False)
+                    
                 if not found:
                     print(f"{user_stdin[1]}: not found")
+        elif PATH:
+            found, search_path = type_command(main_command,all_paths)
+            if found:
+                os.system(command)    
         else:
             print(f"{command}: command not found")
 
