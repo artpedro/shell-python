d4094f3945042106e854eba0b2808edf14b32a4d
Artur Pedro <arturpedromartins@gmail.com>
2025-01-09 00:55:54 -0300

    Feat: REPL implemented

--- app/main.py
+++ app/main.py
@@ -3,16 +3,16 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    
-    valid_commands = ["cd", "pwd", "echo", "exit"]
-    # Wait for user input
-    command = input()
-    if command in valid_commands:
-        print(command)
-    else:
-        print(f"{command}: command not found")
-
+    while True:
+        sys.stdout.write("$ ")
+        
+        valid_commands = ["cd", "pwd", "echo", "exit"]
+        # Wait for user input
+        command = input()
+        if command in valid_commands:
+            print(command)
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
